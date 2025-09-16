4fa8c86bae73ac698d5350b7f99c21e4e52e345b
KaulWaiting <parkerwlee@gmail.com>
2025-09-15 18:03:46 -0700

    Update run.py

--- run.py
+++ run.py
@@ -5,6 +5,4 @@ from app import create_app
 if __name__ == "__main__":
     app = create_app()
     port = int(os.environ.get("PORT", "5000"))
-    # For local kiosk usage the built-in server is fine.
-    # If you later want Socket.IO or gunicorn, we can swap this line.
     app.run(host="0.0.0.0", port=port)
